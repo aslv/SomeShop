683eab22139ab7d799495f4892afa248b8702eb1
Lari <gilarion@yahoo.com>
2013-03-18 10:48:54 +0200

    Създаване на празен клиент и сървис.
    Създадохме празен клиент и сървис. В сървиса направихме една тестова
    функция и го свързахме с клиента.

---
+++ OnlineStoreClient/OnlineStoreClient/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnlineStoreClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServiceReference1.Service1Client _provider = new ServiceReference1.Service1Client();
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbName.Text = _provider.ReturnGay("Pesho");
+        }
+        
+    }
+}
